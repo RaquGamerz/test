9591bf5dbb97ea8fa227b46612144a89cf329983
RaquGamerz <120722735+RaquGamerz@users.noreply.github.com>
2022-12-20 17:05:18 +0530

    Add files via upload

---
+++ Encyclopedia_1/firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAASe2TxoEh5j_pejaJIISxo0OyC3r_MNQ",
+    authDomain: "my-app-1-ea75f.firebaseapp.com",
+    databaseURL: "https://my-app-1-ea75f-default-rtdb.firebaseio.com",
+    projectId: "my-app-1-ea75f",
+    storageBucket: "my-app-1-ea75f.appspot.com",
+    messagingSenderId: "185080970259",
+    appId: "1:185080970259:web:c153cf77db96df9c69d3e6",
+    measurementId: "G-GPWZB54CFP"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
